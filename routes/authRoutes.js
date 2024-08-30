d1f417eee8b62ec6f9469f5a013215082c0cc866
Amir Suhel <amirsuhel5003@gmail.com>
2024-08-30 15:06:43 +0530

    removed console

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { registerUser, authUser } = require('../controllers/authController');
 const router = express.Router();
 
-console.log("aaaa")
 router.post('/register', registerUser);
 router.post('/login', authUser);
 
